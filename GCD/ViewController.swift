0cd0dc3f2e9c93c8109db2f1dbae0787eb93e748
LEEJAEJIN <jinlee1206@naver.com>
2018-08-28 16:50:21 +0900

    동기,비동기 예제추가

--- GCD/ViewController.swift
+++ GCD/ViewController.swift
@@ -9,67 +9,109 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    let imageView = UIImageView()
-    
-    var value = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .white
-        setupViews()
-        changeValue()
+        globalSync()
+        globalAsync()
         
-
+  
     }
-    
-    
-
-
 
 }
 
 extension ViewController {
     
-    fileprivate func setupViews() {
-        
-//        DispatchQueue.global()
-        
-        DispatchQueue.main.async {
+    /*
+     
+     블럭처리된 동기 함수이다.
+     
+     printNumber : 0
+     printNumber : 1
+     printNumber : 2
+     .
+     .
+     .
+     .
+     printNumber :  99
+     printNumber :  100
+     printNumber finish
+     finish globalSync
+     
+     로그를 확인해보면 함수가 끝날때 찍히는 finish globalSync 가 제일 마지막에 불리어진다.
+     
+    */
+    
+    fileprivate func globalSync() {
+
+        DispatchQueue.global().sync {
+            
+            self.printNumber()
             
         }
+
+        print("finish globalSync")
+
+    }
+    
+    
+    /*
+     
+     논블럭처리된 비동기 함수이다.
+     
+     finish globalAsync
+     printNumber :  0
+     printNumber :  1
+     printNumber :  2
+     .
+     .
+     .
+     .
+     printNumber :  99
+     printNumber :  100
+     printNumber finish
+     
+     위에 블럭된 동기함수와 다르게 비동기 함수는 finish Func 이 제일먼저 찍히고
+     async 클로저 스코프안에있는 함수(printNumber)가 실행되고 프린트문이 찍히게된다.
+     
+    */
+    
+    fileprivate func globalAsync() {
         
         DispatchQueue.global().async {
             
-            self.view.addSubview(self.imageView)
-            self.imageView.backgroundColor = .red
-            self.imageView.translatesAutoresizingMaskIntoConstraints = false
-            self.imageView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
-            self.imageView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
-            self.imageView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
-            self.imageView.heightAnchor.constraint(equalToConstant: self.view.frame.width).isActive = true
+            self.printNumber()
             
         }
         
+        print("finish globalAsync")
+        
+    }
+    
+    
+    func printNumber() {
 
-        
-    }
-    
-    
-    fileprivate func changeValue() {
-        
-        DispatchQueue.main.async {
+        for i in 0...100 {
             
-            self.value = 5
+            print("printNumber : ",i)
             
         }
         
-        
+        print("printNumber finish")
         
     }
+
     
 }
+
+
+
+
+
+
+
+
 
 
 
